967863f78be1cb210cb3b5a77e46b9e882e41333
BlisterBoy <mehdi.abelha@gmail.com>
2013-03-22 20:22:17 +0100

    Prototype de la classe Labyrinthe
    Ebauche de la classe Labyrinthe
    Elle prend en parametre une chaine de caractère qui correspond au contenu d'un fichier type
    Elle contient un tableau à deux dimensions contenant des entiers, 0 pour vide, 1 pour mur et 2 pour Sortie.

---
+++ src/Labyrinthe.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import string
+
+class Labyrinthe :
+    """Contient "labyrinthe" : le tableau à deux dimensions représentant le labyrinthe"""
+    def __init__(self, chaine) :
+        """Lis la chaine de caractère chaine et construit un labyrinthe sous forme d'un tableau à deux dimensions"""
+        self.labyrinthe = list()
+        chaine = chaine.split("\n")
+        for ligne in chaine :
+            #On enleve les eventuels espaces incongrus de la chaine
+            ligneLabyrinthe = list()
+            for lettre in ligne :
+                if lettre == "0" :
+                    ligneLabyrinthe.append(0)
+                elif lettre == "1" :
+                    ligneLabyrinthe.append(1)
+                elif lettre == "E" :
+                    ligneLabyrinthe.append(2)
+            self.labyrinthe.append(ligneLabyrinthe)
+            
+    def __str__(self) :
+        chaine = ""
+        for i in self.labyrinthe :
+            chaine += str(i)
+            chaine += "\n"
+        return chaine
