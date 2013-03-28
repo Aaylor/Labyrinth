1a11fb8346df14e0c9c40246fee3ffc411c16b4e
BlisterBoy <mehdi.abelha@gmail.com>
2013-03-28 19:23:56 +0100

    Alpha affichage (manque les bord droit et bas)

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,11 @@
-#!/dev/bin/python
+#!/usr/bin/env python3
+
 from MainFrame import *
 
 if __name__ == "__main__":
     fenetre = MainFrame()
     #fenetre.gameScreen.noGame()
-    #fenetre.gameScreen.topView()
-    fenetre.gameScreen.fpsView()
+    fenetre.gameScreen.topView()
+    #fenetre.gameScreen.fpsView()
     fenetre.mainloop()
 
