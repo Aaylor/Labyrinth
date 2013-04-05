185685d3b1322aa11fae8fe3eb6ecf7b51139584
BlisterBoy <mehdi.abelha@gmail.com>
2013-04-05 11:23:07 +0200

    Nettoyage du code de input_area

--- src/input_area.py
+++ src/input_area.py
@@ -3,27 +3,53 @@ from tkinter import *
 class InputArea(Frame) :
     def __init__(self, mainFrame) :
         Frame.__init__(self, mainFrame)
-        #Bon Changer de Vue
-        displayFrame = Frame(self)
-        displayFrame.pack(side=BOTTOM)
-        displayButton = Button(displayFrame, text="Changer de vue")
-        displayButton.pack(side=RIGHT)
+        self.no_game_controls = self.no_game_controls()
+        self.top_view_controls = self.top_view_controls()
+        self.fps_view_controls = self.fps_view_controls()
 
+    def no_game_controls(self) :
+        frame = Frame(self)
         
-        directionFrame = Frame(self, pady = 5)
+        #Bouton Generer Labyrinthe
+        genererButton = Button(frame, text="Générer un Labyrinthe")
+        genererButton.pack(side=LEFT)
+
+        #Bouton Ouvrir Labyrinthe
+        ouvrirButton = Button(frame, text="Ouvrir un Labyrinthe")
+        ouvrirButton.pack(side=RIGHT)
+
+        return frame
+
+    def top_view_controls(self) :
+        #Construction des Frames
+        frame = Frame(self)
+        displayFrame = Frame(frame)
+        displayFrame.pack(side=BOTTOM)
+        directionFrame = Frame(frame, pady = 5)
         directionFrame.pack(side=BOTTOM)
-        #Bouton Haut
         upFrame = Frame(directionFrame)
         upFrame.pack(side=TOP)
+        bottomFrame = Frame(directionFrame)
+        bottomFrame.pack(side=BOTTOM)
+        
+        #Bouton Changer de Vue
+        displayButton = Button(displayFrame, text="Changer de vue")
+        displayButton.pack(side=RIGHT)
+        
+
+        #Bouton Haut
         upButton = Button(upFrame, text="Haut", width=7)
         upButton.pack()
 
         #Bouton Gauche, Bas, Droite
-        bottomFrame = Frame(directionFrame)
-        bottomFrame.pack(side=BOTTOM)
         leftButton = Button(bottomFrame, text="Gauche", width=7)
         leftButton.pack(side=LEFT) 
         downButton = Button(bottomFrame, text="Bas", width=7)
         downButton.pack(side=LEFT)
         rightButton = Button(bottomFrame, text="Droit", width=7)
         rightButton.pack(side=LEFT)
+
+        return frame
+
+    def fps_view_controls(self) :
+        pass
