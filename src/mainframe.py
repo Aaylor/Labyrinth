836364821fd37ae5f845c9b21909ff93a639ec61
BlisterBoy <mehdi.abelha@gmail.com>
2013-04-14 15:20:38 +0200

    Labyrinthe personalisé opérationnel

--- src/mainframe.py
+++ src/mainframe.py
@@ -5,123 +5,165 @@ from input_area import *
 from gamescreen import *
 
 class MainFrame(Tk) :
-    def __init__(self) :
-        Tk.__init__(self)
+	def __init__(self) :
+		Tk.__init__(self)
 
-        #MenuBar
-        self.menubar = MenuBar(self)
-        self.menubar.pack()
+		#MenuBar
+		self.menubar = MenuBar(self)
+		self.menubar.pack()
 
-        #L'objet jeu
-        self.game = None
+		#L'objet jeu
+		self.game = None
+
+		#Zone d'Affichage
+		self.gamescreen = GameScreen(self)
+		self.gamescreen.no_game()
+		self.gamescreen.pack()
+
+		#Zone de Controle
+		self.inputArea = InputArea(self)
+		self.inputArea.pack()
+		self.inputArea.display_no_game()
+
+
+		#Raccourcis clavier
+		self.bind('<Control-o>', self.menubar.ouvrirFichier)
+		self.bind('<Control-g>', self.menubar.genererLabyrinthe)
+		self.bind('<Control-Shift-G>', self.menubar.genererLabyrinthePersonnalise)
+		self.bind('<v>', self.change_view)
+		self.bind('<Up>', self.move_up)
+		self.bind('<Down>', self.move_down)
+		self.bind('<Left>', self.move_left)
+		self.bind('<Right>', self.move_right)
         
-        #Zone d'Affichage
-        self.gamescreen = GameScreen(self)
-        self.gamescreen.no_game()
-        self.gamescreen.pack()
+	def no_game() :
+		pass
 
-        #Zone de Controle
-        self.inputArea = InputArea(self)
-        self.inputArea.pack()
-        self.inputArea.display_no_game()
+	#Ouverture d'une session de jeu
 
+	def open_file(self) :
+		filename = filedialog.askopenfilename(parent=None,initialdir="../",title='Veuillez choisir un fichier labyrinthe', filetypes = [('Fichier Labyrinthe', '.lab')])
+		if filename != "" :
+			#On associe le jeu à la fenetre
+			self.game = game.game(False, filename=filename)
+			if self.game.game_labyrinth.create == True :
+				self.init_top_view()
+			else :
+				messagebox.showinfo("Fichier .lab invalide", "Le fichier Labyrinthe que vous avez séléctionné n'est pas valide.\nVeuillez ouvrir un autre fichier.")
 
-        #Raccourcis clavier
-        self.bind('<Control-o>', self.menubar.ouvrirFichier)
-        self.bind('<Control-g>', self.menubar.genererLabyrinthe)
-        self.bind('<v>', self.change_view)
-        self.bind('<Up>', self.move_up)
-        self.bind('<Down>', self.move_down)
-        self.bind('<Left>', self.move_left)
-        self.bind('<Right>', self.move_right)
-        
-    def no_game() :
-        pass
+	def generate_labyrinth(self) :
+		#On associe le jeu à la fenetre
+		self.game = game.game(True, width=20, height=10)
+		self.init_top_view()
 
-    #Ouverture d'une session de jeu
+	def generate_labyrinth_perso(self, hauteur, longueur):
+		#On associe le jeu à la fenetre
+		self.game = game.game(True, width=hauteur, height=longueur)
+		self.init_top_view()
+		
+	#Methode de vues
 
-    def open_file(self) :
-        filename = filedialog.askopenfilename(parent=None,initialdir="../",title='Veuillez choisir un fichier labyrinthe', filetypes = [('Fichier Labyrinthe', '.lab')])
-        if filename != "" :
-            #On associe le jeu à la fenetre
-            self.game = game.game(False, filename=filename)
-            if self.game.game_labyrinth.create == True :
-                self.init_top_view()
-            else :
-                messagebox.showinfo("Fichier .lab invalide", "Le fichier Labyrinthe que vous avez séléctionné n'est pas valide.\nVeuillez ouvrir un autre fichier.")
+	def change_view(self, *arg) :
+		if self.gamescreen.mode == "top_view" :
+			self.init_fps_view()
+		elif self.gamescreen.mode == "fps_view" :
+			self.init_top_view()
 
-    def generate_labyrinth(self) :
-        #On associe le jeu à la fenetre
-        self.game = game.game(True, width=20, height=10)
-        self.init_top_view()
+	def init_top_view(self) :
+		self.game.display() #A ENLEVER UNE FOIS QUE L'AFFICHAGE SERA NIQUEL
+		#On passe la zone de controle en mode vue du dessus
+		self.inputArea.display_top_view()
+		#On passe l'ecran de jeu en mode jeu en vue du dessus
+		self.gamescreen.init_top_view()
+		self.gamescreen.draw()
 
-    #Methode de vues
-
-    def change_view(self, *arg) :
-        if self.gamescreen.mode == "top_view" :
-            self.init_fps_view()
-        elif self.gamescreen.mode == "fps_view" :
-            self.init_top_view()
-
-    def init_top_view(self) :
-        self.game.display() #A ENLEVER UNE FOIS QUE L'AFFICHAGE SERA NIQUEL
-        #On passe la zone de controle en mode vue du dessus
-        self.inputArea.display_top_view()
-        #On passe l'ecran de jeu en mode jeu en vue du dessus
-        self.gamescreen.init_top_view()
-        self.gamescreen.draw()
-
-    def init_fps_view(self) :
-        self.inputArea.display_fps_view()
-        self.gamescreen.init_fps_view()
-        self.gamescreen.draw()
+	def init_fps_view(self) :
+		self.inputArea.display_fps_view()
+		self.gamescreen.init_fps_view()
+		self.gamescreen.draw()
 
     
 
-    #Les méthodes de controle du jeu
-    def move(self, direction, *arg) :
-        if self.gamescreen.mode != "no_game" :
-            if self.gamescreen.mode == "top_view" :
-                self.game.move(direction)
-                self.game.player.direction = direction
-            elif self.gamescreen.mode == "fps_view" :
-                if direction == "h" :
-                    self.game.move(self.game.player.direction)
-                elif direction == "b" :
-                    if self.game.player.direction == "h" :
-                        self.game.move("b")
-                    elif self.game.player.direction == "g" :
-                        self.game.move("d")
-                    elif self.game.player.direction == "b" :
-                        self.game.move("h")
-                    elif self.game.player.direction == "d" :
-                        self.game.move("g")
-                elif direction == "g" :
-                    if self.game.player.direction == "h" :
-                        self.game.player.direction = "g"
-                    elif self.game.player.direction == "g" :
-                        self.game.player.direction = "b"
-                    elif self.game.player.direction == "b" :
-                        self.game.player.direction = "d"
-                    elif self.game.player.direction == "d" :
-                        self.game.player.direction = "h"
-                elif direction == "d" :
-                    if self.game.player.direction == "h" :
-                        self.game.player.direction = "d"
-                    elif self.game.player.direction == "g" :
-                        self.game.player.direction = "h"
-                    elif self.game.player.direction == "b" :
-                        self.game.player.direction = "g"
-                    elif self.game.player.direction == "d" :
-                        self.game.player.direction = "b"
-            self.gamescreen.draw()
-            self.game.display()
-                              
-    def move_up(self, *arg) :
-        self.move("h")
-    def move_down(self, *arg) :
-        self.move("b")
-    def move_left(self, *arg) :
-        self.move("g")
-    def move_right(self, *arg) :
-        self.move("d")
+	#Les méthodes de controle du jeu
+	def move(self, direction, *arg) :
+		if self.gamescreen.mode != "no_game" :
+			if self.gamescreen.mode == "top_view" :
+				self.game.move(direction)
+				self.game.player.direction = direction
+			elif self.gamescreen.mode == "fps_view" :
+				if direction == "h" :
+					self.game.move(self.game.player.direction)
+				elif direction == "b" :
+					if self.game.player.direction == "h" :
+						self.game.move("b")
+					elif self.game.player.direction == "g" :
+						self.game.move("d")
+					elif self.game.player.direction == "b" :
+						self.game.move("h")
+					elif self.game.player.direction == "d" :
+						self.game.move("g")
+				elif direction == "g" :
+					if self.game.player.direction == "h" :
+						self.game.player.direction = "g"
+					elif self.game.player.direction == "g" :
+						self.game.player.direction = "b"
+					elif self.game.player.direction == "b" :
+						self.game.player.direction = "d"
+					elif self.game.player.direction == "d" :
+						self.game.player.direction = "h"
+				elif direction == "d" :
+					if self.game.player.direction == "h" :
+						self.game.player.direction = "d"
+					elif self.game.player.direction == "g" :
+						self.game.player.direction = "h"
+					elif self.game.player.direction == "b" :
+						self.game.player.direction = "g"
+					elif self.game.player.direction == "d" :
+						self.game.player.direction = "b"
+			self.gamescreen.draw()
+			self.game.display()
+							  
+	def move_up(self, *arg) :
+		self.move("h")
+	def move_down(self, *arg) :
+		self.move("b")
+	def move_left(self, *arg) :
+		self.move("g")
+	def move_right(self, *arg) :
+		self.move("d")
+        
+        
+        
+class CreateLabFrame(Toplevel) :
+	def __init__(self, mainframe, *arg) :
+		self.mainframe = mainframe
+		Toplevel.__init__(self, mainframe)
+		longueurLab = Label(self, text="Longueur du Labyrinthe :")
+		longueurLab.pack()
+		self.longueurBox = Spinbox(self, from_=10, to=60)
+		self.longueurBox.pack()
+		hauteurLab = Label(self, text="Hauteur du Labyrinthe :")
+		hauteurLab.pack()
+		self.hauteurBox = Spinbox(self, from_=10, to=60)
+		self.hauteurBox.pack()
+		okButton = Button(self, text ="Generer le Labyrinthe", command=self.verify_values)
+		okButton.pack()
+
+	def verify_values(self) :
+		try :
+			longueur = int(self.longueurBox.get())
+			hauteur = int(self.hauteurBox.get())
+			if(longueur > 60 or hauteur > 60) :
+				self.warning_value_too_high()
+			else :
+				self.mainframe.generate_labyrinth_perso(longueur, hauteur)
+				self.destroy()
+		except :
+			self.warning_value_non_correct()
+	
+	def warning_value_too_high(self) :
+		messagebox.showinfo("Erreur de saisie", "Les valeurs saisies sont trop élevées")
+
+	def warning_value_non_correct(self) :
+		messagebox.showinfo("Erreur de saisie", "Les valeurs saisies comportent des caractères non autorisés")
+		
--- src/menubar.py
+++ src/menubar.py
@@ -5,46 +5,51 @@ from tkinter import messagebox
 from tkinter import filedialog
 import game
 import sys
+import mainframe
 
 class MenuBar(Frame) :
-    def __init__(self, mainFrame) :
-        Frame.__init__(self, mainFrame)
-        self.mainFrame = mainFrame
-        self.menuBar = Menu(self)
-        self.master.config(menu=self.menuBar)
+	def __init__(self, mainFrame) :
+		Frame.__init__(self, mainFrame)
+		self.mainFrame = mainFrame
+		self.menuBar = Menu(self)
+		self.master.config(menu=self.menuBar)
 
-        #Menu Fichier
-        fichierMenu = Menu(self, tearoff = 0)
-        self.menuBar.add_cascade(label="Fichier", menu=fichierMenu)
-        fichierMenu.add_command(label="Générer un Labyrinthe", command= self.genererLabyrinthe, accelerator="Ctrl-G")
-        fichierMenu.add_command(label="Ouvrir un Labyrinthe", command = self.ouvrirFichier, accelerator="Ctrl-O")
-        fichierMenu.add_separator()
-        fichierMenu.add_command(label="Quitter", command=self.quitter)
-        
-        #Menu Jeu
-        jeuMenu = Menu(self, tearoff = 0)
-        self.menuBar.add_cascade(label="Jeu", menu=jeuMenu)
-        jeuMenu.add_command(label="Changer de vue", command= self.mainFrame.change_view, accelerator="v")
-        
-        #Menu Aide
-        aideMenu=Menu(self, tearoff=0)
-        self.menuBar.add_cascade(label="Aide", menu=aideMenu)
-        aideMenu.add_command(label="A propos", command=self.aPropos)
+		#Menu Fichier
+		fichierMenu = Menu(self, tearoff = 0)
+		self.menuBar.add_cascade(label="Fichier", menu=fichierMenu)
+		fichierMenu.add_command(label="Générer un Labyrinthe automatiquement", command= self.genererLabyrinthe, accelerator="Ctrl-G")
+		fichierMenu.add_command(label="Générer un Labyrinthe personnalisé", command= self.genererLabyrinthePersonnalise, accelerator="Ctrl-Shift-G")
+		fichierMenu.add_command(label="Ouvrir un Labyrinthe", command = self.ouvrirFichier, accelerator="Ctrl-O")
+		fichierMenu.add_separator()
+		fichierMenu.add_command(label="Quitter", command=self.quitter)
+		
+		#Menu Jeu
+		jeuMenu = Menu(self, tearoff = 0)
+		self.menuBar.add_cascade(label="Jeu", menu=jeuMenu)
+		jeuMenu.add_command(label="Changer de vue", command= self.mainFrame.change_view, accelerator="v")
+		
+		#Menu Aide
+		aideMenu=Menu(self, tearoff=0)
+		self.menuBar.add_cascade(label="Aide", menu=aideMenu)
+		aideMenu.add_command(label="A propos", command=self.aPropos)
 
-    def aPropos(self) :
-        import random
-        if random.randint(0, 1) == 0 :
-            chaine = "\tMehdi Khelifi\n\tRunarvot Loic"
-        else :
-            chaine = "\tRunarvot Loic\n\tMehdi Khelifi"        
-        messagebox.showinfo("A propos", "Dungeon and Python est un projet universitaire développé par deux étudiants : \n" + chaine)
+	def aPropos(self) :
+		import random
+		if random.randint(0, 1) == 0 :
+			chaine = "\tMehdi Khelifi\n\tRunarvot Loic"
+		else :
+			chaine = "\tRunarvot Loic\n\tMehdi Khelifi"        
+		messagebox.showinfo("A propos", "Dungeon and Python est un projet universitaire développé par deux étudiants : \n" + chaine)
 
-    def genererLabyrinthe(self, *arg) :
-        self.mainFrame.generate_labyrinth()
+	def genererLabyrinthe(self, *arg) :
+		self.mainFrame.generate_labyrinth()
 
-    
-    def ouvrirFichier(self, *arg) :
-        self.mainFrame.open_file()
+	def genererLabyrinthePersonnalise(self, *arg) :
+		mainframe.CreateLabFrame(self.mainFrame);
+		
+	
+	def ouvrirFichier(self, *arg) :
+		self.mainFrame.open_file()
 
-    def quitter(self) :
-        sys.exit()
+	def quitter(self) :
+		sys.exit()
